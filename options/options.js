6121c6eda0d1a891747284b6a2ebd264fdae521a
Crowdbotics <engineering@crowdbotics.com>
2023-03-14 15:02:15 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -5,8 +5,10 @@ export const globalOptions = {
   url: "https://dantest20230314-dev-69509.botics.co",
   api: "https://dantest20230314-dev-69509.botics.co/api/v1"
 }
+
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
-  }
+  },
+  undefined: { title: "App Menu", copy: "Routes available!", styles: styles2 }
 }
